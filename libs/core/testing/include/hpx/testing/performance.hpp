a5fa8122c6c92579bcdabb5978252cb98743e920
Vedant <vedantnimjed@gmail.com>
2024-06-01 19:07:40 +0530

    Added initial perftests CI config for displaying plots in CDash
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- libs/core/testing/include/hpx/testing/performance.hpp
+++ libs/core/testing/include/hpx/testing/performance.hpp
@@ -14,12 +14,12 @@
 #include <string>
 
 namespace hpx::util {
-
     HPX_CXX_CORE_EXPORT HPX_CORE_EXPORT inline bool detailed_;
+    HPX_CXX_CORE_EXPORT HPX_CORE_EXPORT inline std::string test_name_;
     HPX_CXX_CORE_EXPORT HPX_CORE_EXPORT void perftests_cfg(
         hpx::program_options::options_description& cmdline);
     HPX_CXX_CORE_EXPORT HPX_CORE_EXPORT void perftests_init(
-        hpx::program_options::variables_map const& vm);
+        hpx::program_options::variables_map const& vm, const std::string test_name);
 #if defined(HPX_HAVE_NANOBENCH)
     HPX_CXX_CORE_EXPORT HPX_CORE_EXPORT void perftests_report(
         std::string const& name, std::string const& exec,
