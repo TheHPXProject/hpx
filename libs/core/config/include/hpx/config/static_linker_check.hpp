55821138202a00af3d06cb84d0d35a969ce75ba9
Hackathon User <hackathon@example.com>
2026-05-04 00:17:10 +0530

    fix: add missing static_linker_check.hpp and resolve CI failures
    Signed-off-by: Shivansh Singh <singhshivansh023@gmail.com>

---
+++ libs/core/config/include/hpx/config/static_linker_check.hpp
@@ -0,0 +1,15 @@
+//  Copyright (c) 2026 The STE||AR-Group
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config/export_definitions.hpp>
+
+#if defined(HPX_HAVE_DYNAMIC_HPX_MAIN)
+#if (defined(__linux) || defined(__linux__) || defined(linux) ||                   defined(__APPLE__)) &&                                                         defined(HPX_HAVE_STATIC_LINKING) &&                                            !defined(HPX_HAVE_WRAP_MAIN_CONFIGURED)
+#warning "You are statically linking HPX on Linux/macOS while using hpx_main.hpp. Please ensure you manually configure the linker to use wrap_main, or use the CMake target HPX::wrap_main to avoid linking errors."
+#endif
+#endif
